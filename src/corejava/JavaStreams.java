229da220ac3f643bd9096fdef504fad03044adc2
prashanthie1991 <prashanthi.e1991@gmail.com>
2023-11-29 16:55:09 +0530

    feat: java streams logic updated

--- src/corejava/JavaStreams.java
+++ src/corejava/JavaStreams.java
@@ -8,16 +8,22 @@ public class JavaStreams {
 
 	public static void main(String[] args) {
 		// Print Unique numbers and Sort the array
-		
-		List<Integer> numbers = Arrays.asList(1,2, 5, 6, 8, 8, 10, 4, 5, 11,1);
-		
-		//Find unique numbers
+
+		List<Integer> numbers = Arrays.asList(1, 2, 5, 6, 8, 8, 10, 4, 5, 11, 1);
+
+		// or
+
+		// Converting integer array to list of integers
+		int[] intArray = { 1, 2, 5, 6, 8, 8, 10, 4, 5, 11, 1 };
+		List<Integer> nlist = Arrays.stream(intArray).boxed().toList();
+
+		// Find unique numbers
 		System.out.println("Unique numbers in the array: ");
 		numbers.stream().distinct().forEach(number -> System.out.print(number + " "));
 		System.out.println();
-		//Sort the array
+		// Sort the array
 		List<Integer> sortedList = numbers.stream().distinct().sorted().collect(Collectors.toList());
-		System.out.println("Sorted array and 3rd index number is: "+sortedList.get(2));
+		System.out.println("Sorted array and 3rd index number is: " + sortedList.get(2));
 
 	}
 
