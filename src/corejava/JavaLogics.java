c686f9b72b747d31a016c0c0bd1a88aa69383c4b
prashanthie1991 <prashanthi.e1991@gmail.com>
2023-11-30 21:53:56 +0530

    feat: logic for array sort

--- src/corejava/JavaLogics.java
+++ src/corejava/JavaLogics.java
@@ -1,5 +1,9 @@
 package corejava;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class JavaLogics {
 
 	public static void main(String[] args) {
@@ -28,6 +32,12 @@ public class JavaLogics {
 		}
 
 		System.out.println();
+		
+		//Or
+		int[] ar = { 3, 7, 4, 8, 2, 4, 1 };
+		Arrays.stream(ar).boxed().sorted().collect(Collectors.toList()).forEach(s -> System.out.print(s + " "));
+		
+		
 
 		// Logic2: Reverse the string
 		String name = "Prashanthi";
